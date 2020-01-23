adee9d653761ff3c155ab64c1b3a8346ff68a539
hjunkim <hjunkim@skku.edu>
2020-01-23 15:41:14 +0900

    first version commit

--- refactor.py
+++ refactor.py
@@ -8,22 +8,33 @@ PAGE_MASK=(~(PAGE_SIZE-1))
 
 WARP_SIZE=32 # warp size: 32
 
-def refactor(conts, fpw):
+def refactor(conts, file_name):
 	epoch = 0
 	epoch_i = 0
 	kernel_name = ''
+	i_kernel = 0
 	grids_sum = 1
 	blocks_sum = 1
 	t_addr_list = list()
+	is_first_kernel=True
 	
 	for line in conts:
 
 		# Kernel info., calculate # warps
-		if 'Kernel' in line:
+		if 'Kernel' and 'shmem' and 'nregs' in line:
 			toks = re.split('-', line)
-			kernel_name = toks[0]
+			t_kernel_name = re.split(' ', toks[0])
+			kernel_name = re.split('[\\(]', t_kernel_name[1])[0]
+			print (kernel_name)
 			grids_sum = 1
 			blocks_sum = 1
+
+			if is_first_kernel is True:
+				is_first_kernel=False
+			else:
+				fpw.close()
+			
+			fpw = open(str(i_kernel)+'_'+kernel_name+'_'+file_name, 'w')
 
 			grids = re.split(',', toks[1])
 			grids[0]=re.split(' ', grids[0])[3]
@@ -36,14 +47,14 @@ def refactor(conts, fpw):
 				blocks_sum *= int(b)
 
 			epoch = grids_sum * blocks_sum / WARP_SIZE
+			i_kernel += 1
 		
 		# 
 		if 'CTA' in line:
 			toks = re.split('-', line)
 			addr_list=re.split(' ', toks[3])
 			page_num=''
-			# addr_list=list(set(addr_list))
-			# print(addr_list)
+			addr_list=list(set(addr_list))
 			
 			for addr in addr_list:
 				if '0x' in addr:
@@ -54,20 +65,20 @@ def refactor(conts, fpw):
 						t_addr_list.append(t_str)
 			epoch_i += 1
 
+	fpw.close()
+
 
 def main():
 	if len(sys.argv) < 2:
-		sys.stderr.write('./refactor.py <filename>.cu\n')
+		sys.stderr.write('./refactor.py <filename>.trace\n')
 		exit()
 
 	file_name = str(sys.argv[1])
-	fpw = open('_'+file_name, 'w')
 	with open(file_name, "r") as fpr:
 		contents = fpr.readlines()
-		refactor(contents, fpw)
+		refactor(contents, file_name)
 
 	fpr.close()
-	fpw.close()
 	
 if __name__ == '__main__':
 	main()
